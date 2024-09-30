18b5d89703f8be1fe0e45e8d6f06d10fa56ac865
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-09-30 08:35:02 -0300

    L

--- perguntas.js
+++ perguntas.js
@@ -1,120 +1,120 @@
 criaCartao(
-    'lingua portuguesa',
-    'o que é o romantismo?',
+    'Lingua Portuguesa',
+    'O que é o romantismo?',
     'Movimento literário que valoriza o subjetivismo e os sentimentos'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'característas do romantismo?',
+    'Lingua Portuguesa',
+    'Característas do romantismo?',
     'Idealização do amor, natureza, individualismo e liberdade'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'qual período do romatismo?',
+    'Lingua Portuguesa',
+    'Qual período do romatismo?',
     'Início no início do século XIX, aproximadamente 1836.'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais os principais autores do romantismo?',
+    'Lingua Portuguesa',
+    'Quais os principais autores do romantismo?',
     'Álvares de Azevedo, Castro Alves, José de Alencar'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'qual principal obra do romantismo',
-    'Iracema" de José de Alencar'
+    'Lingua Portuguesa',
+    'Qual principal obra do romantismo',
+    '"Iracema" de José de Alencar'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais foram as influências?',
+    'Lingua Portuguesa',
+    'Quais foram as influências?',
     'Revolução Francesa, romantismo europeu'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais estilos literários abrange o romantrismo?',
-    ' Poesia, romance, teatro'
+    'Lingua Portuguesa',
+    'Quais estilos literários abrange o romantrismo?',
+    'Poesia, romance, teatro'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais temas são recorrentes no romantismo?',
+    'Lingua Portuguesa',
+    'Quais temas são recorrentes no romantismo?',
     'Nacionalismo, melancolia, conflito entre razão e emoção'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'o que é o barroco?',
-    ' Movimento artístico e literário que expressa complexidade e dualidade'
+    'Lingua Portuguesa',
+    'O que é o barroco?',
+    'Movimento artístico e literário que expressa complexidade e dualidade'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'qual período do barroco?',
+    'Lingua Portuguesa',
+    'Qual período do barroco?',
     'Século XVII, principalmente entre 1600 e 1750'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais as características do barroco?',
+    'Lingua Portuguesa',
+    'Quais as características do barroco?',
     'Ornamentação excessiva, antítese, paradoxo'
 
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais os principais autores do barroco?',
+    'Lingua Portuguesa',
+    'Quais os principais autores do barroco?',
     'Gregório de Matos, Padre Vieira'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'qual a obra emblemática do barroco?',
+    'Lingua Portuguesa',
+    'Qual a obra emblemática do barroco?',
     '"A Arte de Furtar" de Gregório de Matos'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais as influencias do barroco?',
+    'Lingua Portuguesa',
+    'Quais as influencias do barroco?',
     'Contrarreforma, estética europeia'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais as influencias do barroco?',
+    'Lingua Portuguesa',
+    'Quais as influencias do barroco?',
     'Poesia, prosa, sermões'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'quais os temas recorrentes do barroco?',
+    'Lingua Portuguesa',
+    'Quais os temas recorrentes do barroco?',
     ' Morte, efemeridade, espiritualidade'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'comparação emtre romantis,o e barroco',
+    'Lingua Portuguesa',
+    'Comparação emtre romantis,o e barroco',
     'Romantismo é subjetivo, enquanto o Barroco é mais complexo e reflexivo'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'comparação entre os estilos do romantismo e barroco',
+    'Lingua Portuguesa',
+    'Comparação entre os estilos do romantismo e barroco',
     'Romantismo usa uma linguagem simples; Barroco, rica em figuras de linguagem.'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'comparação entre a visão do homem no romantismo e no barroco',
+    'Lingua Portuguesa',
+    'Comparação entre a visão do homem no romantismo e no barroco',
     'Romantismo foca no indivíduo; Barroco na condição humana e espiritual'
 )
 
 criaCartao(
-    'lingua portuguesa',
-    'comparação entre o legado do romantismo e do barroco',
+    'Lingua portuguesa',
+    'Comparação entre o legado do romantismo e do barroco',
     'Ambos influenciaram profundamente a literatura brasileira'
 )
