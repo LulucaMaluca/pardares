a4e768c2ea97e434d1c0f2d78e57287a245786f2
LulucaMaluca <163294645+LulucaMaluca@users.noreply.github.com>
2024-09-30 08:15:23 -0300

    ]

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function criaCartao(categoria, pergunta, resposta){
 </div>
 </div>
 `
-container.appendChild(cartao);
 let respostaVisivel = false;
     function viraCartao() {
         respostaVisivel = !respostaVisivel;
